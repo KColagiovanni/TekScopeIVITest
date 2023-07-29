e42e693e244ec89a9226f292b8d402a7d36135a7
kcolagiovanni <KColagiovanni@gmail.com>
2023-07-29 00:44:43 -0700

    Initial Commit

--- TekScopeIVITest/Form1.Designer.cs
+++ TekScopeIVITest/Form1.Designer.cs
@@ -28,13 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tek_lbl1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tek_lbl2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tek_lbl3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Record Legnth?";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(316, 164);
+            this.listBox1.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tek_lbl1,
+            this.tek_lbl2,
+            this.tek_lbl3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 271);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(535, 26);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tek_lbl1
+            // 
+            this.tek_lbl1.Name = "tek_lbl1";
+            this.tek_lbl1.Size = new System.Drawing.Size(37, 20);
+            this.tek_lbl1.Text = "tek1";
+            // 
+            // tek_lbl2
+            // 
+            this.tek_lbl2.Name = "tek_lbl2";
+            this.tek_lbl2.Size = new System.Drawing.Size(37, 20);
+            this.tek_lbl2.Text = "tek2";
+            // 
+            // tek_lbl3
+            // 
+            this.tek_lbl3.Name = "tek_lbl3";
+            this.tek_lbl3.Size = new System.Drawing.Size(37, 20);
+            this.tek_lbl3.Text = "tek3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(535, 297);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "TekSCopeIVITest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tek_lbl1;
+        private System.Windows.Forms.ToolStripStatusLabel tek_lbl2;
+        private System.Windows.Forms.ToolStripStatusLabel tek_lbl3;
     }
 }
 
--- TekScopeIVITest/Form1.cs
+++ TekScopeIVITest/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TVCLib;
 
 namespace TekScopeIVITest
 {
@@ -15,6 +16,37 @@ namespace TekScopeIVITest
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        Tvc tvc1 = new Tvc();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Array TekList;
+
+            try
+            {
+                tvc1.SearchCriterion = 0;
+                TekList = tvc1.FindList as Array;
+                foreach (object item in TekList)
+                {
+                    listBox1.Items.Add(item);
+                }
+
+                listBox1.SetSelected(0, true);
+                tvc1.Descriptor = Convert.ToString(listBox1.SelectedItem);
+//                tek_lbl1.Text = "MFG: " + tvc1.InstrumentManufacturer.ToString();
+//                tek_lbl1.Text = "Model: " + tvc1.InstrumentModel.ToString();
+//                tek_lbl1.Text = "SID: " + tvc1.SoftwareManufacturerID.ToString();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\r\n" + "No device found!");
+            }
+
         }
     }
 }
